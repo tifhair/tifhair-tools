037bbd56c0e4203da348eb4b3e80e8e82138aeb5
Tif Hair <89245044+tifhair@users.noreply.github.com>
2022-03-14 18:02:40 +0100

    update for new schema

--- tools/blague.rb
+++ tools/blague.rb
@@ -1,46 +1,96 @@
+require "set"
 require "sqlite3"
+require 'optparse'
+require "progressbar"
 
-dbfile = ARGV[0]
 
-if not dbfile or not File.exist?(dbfile)
-  raise "Usage: ruby blague.rb <sqlite>"
+def usage
+  puts "Usage: ruby blague.rb --db=<sqlite> [--bad=file1] [--good=file2]"
+  puts "   --db pointe vers le fichier SQLite3 des coiffeurs généré avec sirene.rb"
+  puts "   [--bad] est un fichier optionel contenant une liste de noms connus pour ne pas être drôle"
+  puts "   [--good] est un fichier optionel contenant une liste de noms connus pour être hilarant"
+  exit
 end
 
-$blague = 'etat = "A" AND (name LIKE "%tif%" OR name LIKE "%hair%" OR name LIKE "%epi%" OR name LIKE "%mech%" OR name LIKE "%tete%" OR name LIKE "%decoif%")'
+
+options = {}
+OptionParser.new do |opt|
+  opt.on('--db FILE') { |o| options[:db] = o }
+  opt.on('--bad FILE') { |o| options[:list_bad] = o }
+  opt.on('--good FILE') { |o| options[:list_good] = o }
+end.parse!
+
+if not options[:db] or not File.exist?(options[:db])
+  pp "Pas de fichier #{options[:db]} trouvé"
+  usage
+end
+dbfile = options[:db]
+
+$bad_chars = /[-\.'&, ]/
+
+$bad_names = []
+if options[:list_bad]
+  if not File.exist?(options[:list_bad])
+    puts "Impossible de trouver le fichier #{options[:list_bad]}"
+    usage
+  end
+  $bad_names = Set.new(File.open(options[:list_bad]).each_line.map{|l| l.gsub($bad_chars, "").strip()}.to_a)
+  puts "Chargé #{$bad_names.size} non blagues"
+end
+
+$good_names = []
+if options[:list_good]
+  if not File.exist?(options[:list_good])
+    puts "Impossible de trouver le fichier #{options[:list_good]}"
+    usage
+  end
+  $good_names = Set.new(File.open(options[:list_good]).each_line.map{|l| l.gsub($bad_chars,"").strip()}.to_a)
+  puts "Chargé #{$good_names.size} blagues"
+end
 db = SQLite3::Database.open(dbfile)
 
-# Les classiques
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%atmos%air%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%hair%du%te%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%imagin%air%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%bull%d%hair%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%ara%t%hair%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%PLANET%HAIR%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%PIL%HAIR%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%infini%tif%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%imagina%tif%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%evolu%tif%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%instinc%tif%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%univ%hair%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%de meche avec%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%beautiful%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%racine%carr%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%au%long%court%'")
-db.execute("UPDATE Coiffeurs set blague=1 WHERE name LIKE '%O%long%court%'")
+def is_blague(name)
+  return false if name.size < 5
+
+  if $good_names.include?(name.gsub($bad_chars,""))
+    return true
+  end
+  if $bad_names.include?(name.gsub($bad_chars,""))
+    return false
+  end
+
+  puts "Nouveau nom #{name} \nEst-ce que c'est drole ? [y/N]"
+  res = $stdin.gets().strip
+
+  if res=~/^n?$/i
+    $bad_names << name.gsub($bad_chars,"")
+    return false
+  end
+  puts "good"
+  $good_names << name.gsub($bad_chars,"")
+  return true
+end
 
 
 i=0
-tab = db.execute("SELECT name, Count(name) as cnt FROM Coiffeurs WHERE #{$blague} AND blague IS NULL GROUP BY name ORDER BY name DESC")
+# All
+likes = ""
+# Potentielles blagues
+#likes = "AND (n.name LIKE "+ ["poux", "poil", "brun", "blon", "dread", "lock","tress", "un chev", "aqua%fur%", "raie", "boucl", "chauv", "tiph","point","frang", "hom", "head", "coup", "long", "cour", "racin", "t'if", "h'air", "fris," "cis", "mech", "mesh", "tyf","mel", "peig", "hair","tif","epi", "tete", "afro"].map{|x| "\"%"+x+"%\""}.join(" OR n.name LIKE ") + ")"
+tab = db.execute("SELECT c.siret, n.name, c.ville FROM Coiffeurs as c, Names as n WHERE c.etat='A' AND blague IS NULL AND c.siret = n.siret #{likes} ORDER BY n.name DESC;")
 total = tab.size()
+progressbar = ProgressBar.create(total: total, format: '%a %e %P% Processed: %c from %C')
 tab.each do |row|
-  puts "#{row[0]}  --- #{row[1]} (#{i}/#{total} #{i*100.0/total})"
+  begin
+    progressbar.progress += 10 if i%10 ==0
+  rescue
+  end
   i+=1
-
-  toto = STDIN.gets()
-  if toto=~/[a-z]{1}/i
-    db.execute("UPDATE Coiffeurs set blague=0 where name = ?", row[0])
+  puts "#{row[2]} "
+  if is_blague(row[1])
+    db.execute("UPDATE Names set blague=1 where name=?", row[1])
   else
-    # ne rien indiquer = blague
-    db.execute("UPDATE Coiffeurs set blague=1 where name = ?", row[0])
+    db.execute("UPDATE Names set blague=0 where name=?", row[1])
   end
 end
+progressbar.finish
